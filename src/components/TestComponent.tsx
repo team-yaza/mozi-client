b0fd12db0c742799bf1172cf72ba87aae0d8d5e3
hyunjin <leehj0110@kakao.com>
2022-07-03 23:02:10 +0900

    test: styled component 작동 테스트

---
+++ src/components/TestComponent.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const TestComponent: React.FC = () => {
+  return <Test>Test Styled Component</Test>;
+};
+
+const Test = styled.div``;
+
+export default TestComponent;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
+import TestComponent from '@src/components/TestComponent';
 import { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return <div>Next.js</div>;
+  return (
+    <div>
+      Next.js
+      <TestComponent />
+    </div>
+  );
 };
 
 export default Home;
