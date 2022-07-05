0252df7b9a03470dda73b93c5a2212f74103c3fc
ChanHui <kinm1596@daum.net>
2022-07-05 14:31:23 +0900

    Revert "chore: Next 세팅 (#8)"
    This reverts commit 99092fe49a55be12ae7f67098b87ed05f449af5e.

---
+++ src/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './App';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLDivElement
+);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
