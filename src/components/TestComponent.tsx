e7d0883dc12c337da9899c7f42d52bb111c8dd14
ChanHui <kinm1596@daum.net>
2022-07-05 14:32:14 +0900

    Revert "chore: basic setting" (#12)
    * Revert "chore: Next 세팅 (#8)"
    
    This reverts commit 99092fe49a55be12ae7f67098b87ed05f449af5e.
    
    * Revert "chore: printWidth 120 (#10)"
    
    This reverts commit 066f71e14bde637f19cba23b938990a8c4e0f5e5.
    
    * Revert "chore: webpack 설정 (#7)"
    
    This reverts commit b0d3de3f298db217d7a417cff4207029566f7f24.
    
    * Revert "chore: TypeScript 세팅 (#6)"
    
    This reverts commit 7c794d33db86418b11589e88c54d07c780efa5fe.
    
    * Revert "chore: lint 설정 (#5)"
    
    This reverts commit a9ab449d44363437db73e8c7285c03aa0b63310e.

--- src/components/TestComponent.tsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const TestComponent: React.FC = () => {
-  return <Test>Test Styled Component</Test>;
-};
-
-const Test = styled.div``;
-
-export default TestComponent;
