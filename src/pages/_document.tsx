d421ebc529f237a6e97fbfeceac411f0ab2b0df8
ChanHui <kinm1596@daum.net>
2022-07-05 14:37:54 +0900

    Front-End Project Setting (#13)
    * chore: lint 설정 (#5)
    
    * chore: TypeScript 세팅 (#6)
    
    * chore: webpack 설정 (#7)
    
    * chore: chore: prettier 설정 추가 (#10)
    
    * chore: Next 세팅 (#8)

---
+++ src/pages/_document.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { ServerStyleSheet } from 'styled-components';
+import Document, { Html, Head, Main, NextScript, DocumentContext, DocumentInitialProps } from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+        });
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: [
+          <React.Fragment key={'key'}>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </React.Fragment>,
+        ],
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta charSet="utf-8" />
+          {/* <link rel="shortcut icon" href="/favicon.svg" type="image/svg" /> */}
+          {/* <link rel="manifest" href="/manifest.json" /> */}
+          {/* <link rel="apple-touch-icon" href="/icon.png" />
+          <meta name="theme-color" content="#fff" /> */}
+          <meta property="og:title" content="" />
+          <meta property="og:image" content="" />
+          <meta property="og:description" content="" />
+          <meta property="og:url" content="/" />
+          <meta name="description" content="" />
+          <meta name="keywords" content="" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
