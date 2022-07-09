fb21c478ed2d0ae3db02fde1aacf446ffa0980dd
hyunjin <leehj0110@kakao.com>
2022-07-09 15:52:20 +0900

    feat: next app에globalStyle 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,18 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { GlobalStyle } from 'styles/globalStyle';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>MOZI</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
