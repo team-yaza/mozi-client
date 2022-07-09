6db0ef415ddf5d329ee98efc65ae1fde4543ddf2
hyunjin <leehj0110@kakao.com>
2022-07-10 03:19:16 +0900

    chore: eslint import 설정 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,17 @@
 module.exports = {
   env: {
-    browser: true,
     es2021: true,
     node: true,
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-  ],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
     ecmaVersion: 13,
     sourceType: 'module',
   },
-  plugins: ['react', '@typescript-eslint'],
+  plugins: ['@typescript-eslint'],
   rules: {
-    indent: ['error', 'space'],
+    indent: ['error', 2],
     'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
     semi: ['error', 'always'],
