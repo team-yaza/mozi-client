f31cf326bebb4b78f99ece441e82949f123f9bf5
hyunjin <leehj0110@kakao.com>
2022-07-10 18:45:16 +0900

    feat: react-query를 사용해 area부분 fetch

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,41 @@
-import TestComponent from 'components/TestComponent';
+import { apiClient } from '@/shared/api/apiClient';
 import { NextPage } from 'next';
+import { useQuery } from 'react-query';
 import styled from 'styled-components';
 
 const Home: NextPage = () => {
+  const { data: areas } = useQuery('areas', async () => apiClient.get('/area/all'));
+  console.log(areas);
   return (
     <Container>
-      Next.js
-      <TestComponent />
+      <Menu>
+        {areas?.data.map((area) => (
+          <div>{area.name}</div>
+        ))}
+      </Menu>
+      <Content>hi2</Content>
     </Container>
   );
 };
 
 const Container = styled.div`
   display: grid;
+  height: 100vh;
+  grid-template-columns: 24rem 1fr;
+`;
+
+const Menu = styled.div`
+  width: 100%;
+  height: 100%;
+
+  font-size: 5rem;
+
+  background-color: #f7f6f3;
+`;
+
+const Content = styled.div`
+  width: 100%;
+  height: 100%;
 `;
 
 export default Home;
