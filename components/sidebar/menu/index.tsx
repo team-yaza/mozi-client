0502f33286844f42d655c0ba9396a593d5fc9cfe
hyunjin <leehj0110@kakao.com>
2022-07-14 16:42:42 +0900

    refactor: sidebar 이름 겹침에 따른 수정 및 타입 추가

--- components/sidebar/menu/index.tsx
+++ components/sidebar/menu/index.tsx
@@ -1,8 +1,14 @@
-import { Container, Icon } from '@/components/sidebar/menu/style';
-import { MenuProps } from '@/shared/types/menu';
 import Link from 'next/link';
 
-export const Menu = ({ title, iconUrl, link }: MenuProps) => {
+import { Container, Icon } from '@/components/sidebar/menu/style';
+
+interface MenuProps {
+  title: string;
+  iconUrl: string;
+  link: string;
+}
+
+const SideBarMenu: React.FC<MenuProps> = ({ title, iconUrl, link }) => {
   return (
     <Container>
       <Icon src={iconUrl} />
@@ -12,3 +18,5 @@ export const Menu = ({ title, iconUrl, link }: MenuProps) => {
     </Container>
   );
 };
+
+export default SideBarMenu;
