41413d6131385507764ae7e3e149c18291cd85b4
hyunjin <leehj0110@kakao.com>
2022-07-21 14:48:44 +0900

    chore: queryClient 옵션 변경

--- shared/utils/queryClient.ts
+++ shared/utils/queryClient.ts
@@ -1,3 +1,11 @@
 import { QueryClient } from 'react-query';
 
-export const queryClient = new QueryClient();
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+      suspense: false,
+    },
+  },
+});
