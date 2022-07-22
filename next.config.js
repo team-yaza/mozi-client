e01e3fcba2d6397db39915efefb4f767db8dba36
HanCiHu <chanhui.yu.cnu@gmail.com>
2022-07-22 10:02:07 +0900

    chore: config 세팅 및 서비스워커 인터페이스 작성

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
+const withPWA = require('next-pwa');
+const withPlugins = require('next-compose-plugins');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   // pageExtensions: ['page.tsx', 'page.tx'],
 };
 
-module.exports = nextConfig;
+module.exports = withPlugins([
+  withPWA({
+    pwa: {
+      dest: 'public',
+    },
+  }),
+  nextConfig,
+]);
