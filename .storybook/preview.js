903e79915529adb8f8124878c3050e2cf6a995ac
hyunjin <leehj0110@kakao.com>
2022-07-27 00:39:44 +0900

    chore: storybook 절대경로 적용 및 globalStyle 적용

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,13 @@
+import { addDecorator } from '@storybook/react';
+import { GlobalStyle } from '../styles/globalStyle';
+
+addDecorator((story) => (
+  <>
+    <GlobalStyle />
+    {story()}
+  </>
+));
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
