01a0853e8a0c225fffa5f8938180e46d01b03cec
hyunjin <leehj0110@kakao.com>
2022-07-26 11:40:43 +0900

    chore: service worker dev log 안찍히게 수정

--- worker/index.ts
+++ worker/index.ts
@@ -4,11 +4,11 @@ declare const self: ServiceWorkerGlobalScope;
 
 // To disable all workbox logging during development, you can set self.__WB_DISABLE_DEV_LOGS to true
 // https://developers.google.com/web/tools/workbox/guides/configure-workbox#disable_logging
-//
-// self.__WB_DISABLE_DEV_LOGS = true
+
+self.__WB_DISABLE_DEV_LOGS = true;
 
 // listen to message event from window
 
 self.addEventListener('fetch', (event) => {
-  console.log('1 : ', event?.request.url);
+  // console.log('1 : ', event?.request.url);
 });
