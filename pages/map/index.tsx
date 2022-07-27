cda5b0b702221be519724b5bde924a39fb208998
HYUNJIN LEE <63354527+hyunjinee@users.noreply.github.com>
2022-07-27 11:13:25 +0900

    feat: 사용자 위치 마킹 (#90)
    * feature: 현재 위치를 바탕으로 네이버 지도에 마킹
    
    * feat: 현재 위치를 가져오는 함수 모듈화

--- pages/map/index.tsx
+++ pages/map/index.tsx
@@ -1,18 +1,21 @@
 import type { NextPage } from 'next';
-import Script from 'next/script';
 import styled from 'styled-components';
 
 import SearchInput from '@/components/map/SearchInput';
 import { useMap } from '@/hooks/useMap';
+import Head from 'next/head';
 
 const Map: NextPage = () => {
   const { mapRef, myLocation, setMyLocation } = useMap();
 
   return (
     <>
-      <Script
-        src={`https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.NEXT_PUBLIC_NAVER_CLIENT_ID}&submodules=geocoder`}
-      ></Script>
+      <Head>
+        <script
+          defer
+          src={`https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.NEXT_PUBLIC_NAVER_CLIENT_ID}&submodules=geocoder`}
+        ></script>
+      </Head>
       <SearchInput />
       <MapLayout id="map" ref={mapRef} />
     </>
