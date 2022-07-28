77155f625b8847cddab05ed8e228e4761d65902d
HanCiHu <chanhui.yu.cnu@gmail.com>
2022-07-28 20:53:16 +0900

    feat: 현재 받아와야 하는 위치를 얻기 위한 커스텀 훅 useLocation

--- hooks/useMap.ts
+++ hooks/useMap.ts
@@ -1,24 +1,9 @@
-import { getCurrentPosition } from '@/shared/utils/getCurrentPosition';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef } from 'react';
+import { useLocation } from './useLocation';
 
 export const useMap = () => {
   const mapRef = useRef<HTMLDivElement>(null);
-  const [myLocation, setMyLocation] = useState<
-    | {
-        latitude: number;
-        longitude: number;
-      }
-    | string
-  >('');
-
-  useEffect(() => {
-    if (navigator.geolocation) {
-      getCurrentPosition(setMyLocation);
-    } else {
-      window.alert('현재 위치를 알 수 없어 기본 위치로 지정합니다.');
-      setMyLocation({ latitude: 37.4862618, longitude: 127.1222903 });
-    }
-  }, []);
+  const { myLocation } = useLocation();
 
   useEffect(() => {
     if (typeof myLocation !== 'string') {
@@ -46,6 +31,5 @@ export const useMap = () => {
   return {
     mapRef,
     myLocation,
-    setMyLocation,
   };
 };
