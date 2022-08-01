49f00fd1071e2f2578fb605067ce9032119492ba
hyunjin <leehj0110@kakao.com>
2022-08-02 00:41:15 +0900

    chore: ErrorBoundary에서 사용하지 않는 코드 제거

--- components/common/ErrorBoundary/index.tsx
+++ components/common/ErrorBoundary/index.tsx
@@ -39,25 +39,3 @@ class ErrorBoundary extends React.Component<Props, State> {
 }
 
 export default ErrorBoundary;
-
-// export default class ErrorBoundary extends React.Component<Props, State> {
-//   public state: State = {};
-
-//   public componentDidCatch(error: Error) {
-//     this.props.onError?.(error);
-//     this.setState({
-//       error,
-//     });
-//   }
-
-//   public render() {
-//     const { error } = this.state;
-//     const { fallback } = this.props;
-
-//     if (error) {
-//       return <>{fallback}</>;
-//     }
-
-//     return this.props.children;
-//   }
-// }
