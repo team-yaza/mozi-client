cd3111b8e551cb8209e3baa15c7cee81f867b27e
HYUNJIN LEE <63354527+hyunjinee@users.noreply.github.com>
2022-08-01 09:11:18 +0900

    feat: useOnClickOutside (#111)
    * test: useOnClickOutside
    
    * feat: useOnClickOutside

---
+++ hooks/useOnClickOutside/index.ts
@@ -0,0 +1,21 @@
+import { RefObject, useEffect } from 'react';
+
+type Handler = (event: MouseEvent | TouchEvent) => void;
+
+export const useOnOutsideClick = <T extends HTMLElement = HTMLElement>(ref: RefObject<T>, handler: Handler) => {
+  useEffect(() => {
+    const listener = (event: MouseEvent | TouchEvent) => {
+      if (!ref.current || ref.current.contains(event.target as HTMLElement)) return;
+
+      handler(event);
+    };
+
+    document.addEventListener('mousedown', listener);
+    document.addEventListener('touchstart', listener);
+
+    return () => {
+      document.removeEventListener('mousedown', listener);
+      document.removeEventListener('touchstart', listener);
+    };
+  }, [ref, handler]);
+};
