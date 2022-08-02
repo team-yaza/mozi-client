779996db829bdf8d7a9a1dec960ab034fa78bcd3
HYUNJIN LEE <63354527+hyunjinee@users.noreply.github.com>
2022-08-02 12:18:54 +0900

    feat: ErrorBoundary (#117)
    * test: ErrorBoundary
    
    * feat: ErrorBoundary

---
+++ components/common/ErrorBoundary/index.test.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from '@testing-library/react';
+
+import ErrorBoundary from '.';
+
+const ComponentThatThrowsError = () => {
+  throw new Error('Bomb');
+};
+
+describe('<ErrorBoundary/>', () => {
+  beforeEach(() => {
+    jest.spyOn(console, 'error').mockImplementation();
+  });
+
+  it('하위 컴포넌트에서 발생한 에러를 캐치 할 수 있다.', () => {
+    render(
+      <ErrorBoundary fallback={<div>에러</div>}>
+        <ComponentThatThrowsError />
+      </ErrorBoundary>
+    );
+
+    expect(screen.getByText(/에러/i)).toBeVisible();
+  });
+});
