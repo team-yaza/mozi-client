f8f99542a741415b902b8081a6aadbdc53f8f0c8
HanCiHu <chanhui.yu.cnu@gmail.com>
2022-08-05 14:48:58 +0900

    refactor: location update api에 맞게 todo service 수정

--- services/apis/todo.ts
+++ services/apis/todo.ts
@@ -1,11 +1,18 @@
 import { Todo } from '@/shared/types/todo';
 import fetcher from '@/shared/utils/fetcher';
 
+interface updateTodoProps {
+  id: string;
+  title?: string;
+  longitude?: number;
+  latitude?: number;
+}
+
 const todoService = {
   createTodo: async (title: string) => await fetcher('post', '/todos', { title }),
   getTodos: async (): Promise<Todo[]> => await fetcher('get', '/todos'),
-  updateTodo: async ({ id, title, description }: { id: string; title?: string; description?: string }) =>
-    await fetcher('patch', `/todos/${id}`, { title }),
+  updateTodo: async ({ id, title, longitude, latitude }: updateTodoProps) =>
+    await fetcher('patch', `/todos/${id}`, { title, longitude, latitude }),
   deleteTodo: async (id: string) => await fetcher('delete', `/todos/${id}`),
 };
 
