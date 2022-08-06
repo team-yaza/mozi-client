b62d846355866bdfb30db921d093e80909748eaa
hyunjin <leehj0110@kakao.com>
2022-08-06 09:17:41 +0900

    fix: contentEditable hook return value 수정에 따른 테스트 수정

--- hooks/useContentEditable/index.test.tsx
+++ hooks/useContentEditable/index.test.tsx
@@ -1,28 +1,28 @@
-import { renderHook, act } from '@testing-library/react';
+import { renderHook, act, render } from '@testing-library/react';
 
 import { useContentEditable } from '.';
 
 describe('useContentEditable', () => {
   it('type check', () => {
     const { result } = renderHook(() => useContentEditable(''));
-    const { content, setContent, onInput, $contentEditable } = result.current;
+    const [content, setContent, $contentEditable, onInput] = result.current;
 
     expect(typeof content).toBe('string');
     expect(typeof setContent).toBe('function');
-    expect(typeof onInput).toBe('function');
     expect(typeof $contentEditable).toBe('object');
+    expect(typeof onInput).toBe('function');
   });
 
   it('초기값이 설정되어야 한다', () => {
     const { result } = renderHook(() => useContentEditable('INITIAL_CONTENT'));
-    const { content } = result.current;
+    const [content] = result.current;
 
     expect(content).toBe('INITIAL_CONTENT');
   });
 
   it('초기값이 주어지지 않으면 빈 문자열이 설정되어야 한다.', () => {
     const { result } = renderHook(() => useContentEditable());
-    const { content } = result.current;
+    const [content] = result.current;
 
     expect(content).toBe('');
   });
@@ -30,15 +30,15 @@ describe('useContentEditable', () => {
   it('HTMLDivElement에 contentEditable 속성을 적용하여 수정할 수 있다.', () => {
     // Arrange
     const { result } = renderHook(() => useContentEditable('INITIAL_CONTENT'));
-    const { setContent, $contentEditable } = result.current;
+    const [, setContent, $contentEditable] = result.current;
 
-    const $divElement = document.createElement('div');
-    $contentEditable.current = $divElement;
+    render(<div ref={$contentEditable} />);
+
     // Act
     act(() => {
       setContent('NEW_CONTENT');
     });
     // Assert
-    expect(result.current.content).toBe('NEW_CONTENT');
+    expect(result.current[0]).toBe('NEW_CONTENT');
   });
 });
