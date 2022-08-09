c8e5b930bcff51b895f5f1540fe7f84686fd13ba
ChanHui <chanhui.yu.cnu@gmail.com>
2022-08-09 13:54:34 +0900

    feat: cache data using service worker (#139)

--- worker/index.ts
+++ worker/index.ts
@@ -9,8 +9,38 @@ self.__WB_DISABLE_DEV_LOGS = true;
 
 // const WB_MANIFEST = self.__WB_MANIFEST;
 
+const CACHE_NAME = 'mozi-v1';
+
 self.addEventListener('fetch', (event) => {
-  // console.log(event?.request.url);
+  event?.respondWith(
+    fetch(event.request)
+      .catch(() => {
+        //오프라인이면 캐시에서 데이터 가져옴
+        return caches.match(event.request).then((response: any) => {
+          //있으면 그거 가져옴
+          if (response) return response;
+          //없으면 ...
+          else {
+            console.log(1);
+          }
+        });
+      })
+      .then(async (reponse: any) => {
+        const cacheResponse = await caches.match(event.request);
+        //있으면 이전 캐시 지우고 최신걸로 업데이트
+        if (cacheResponse) {
+          console.log('있어요.', reponse);
+          return reponse;
+        }
+        //없으면 새로운 캐시 등록
+        else {
+          console.log('없어요', reponse);
+          const cache = await caches.open(CACHE_NAME);
+          cache.add(event.request);
+          return reponse;
+        }
+      })
+  );
 });
 
 self.addEventListener('push', (event) => {
