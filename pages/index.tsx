eda134024a5e998e8ab7593195d5129b783ae1ff
HYUNJIN LEE <63354527+hyunjinee@users.noreply.github.com>
2022-08-10 20:26:40 +0900

    feat: 페이지와 서비스워커간의 통신 구현 (#144)
    * feat: service worker Client type에 visibilityState 추가
    
    * chore: serviceWorker type 정의 파일 삭제
    
    * chore: service worker 와 page 통신 예제코드 추가
    
    * feat: caching strategy with workbox
    
    * feat: 페이지와 서비스워커간의 통신 구현

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import { NextPage } from 'next';
-import { useCallback } from 'react';
+import { useCallback, useEffect } from 'react';
 import styled from 'styled-components';
 
 import Header from '@/components/index/Header';
@@ -14,6 +14,12 @@ const Home: NextPage = () => {
   const createTodoMutation = useCreateTodoMutation();
   const updateTodoMutation = useUpdateTodoMutation();
   const deleteTodoMutation = useDeleteTodoMutation();
+
+  useEffect(() => {
+    if ('serviceWorker' in navigator && navigator.serviceWorker.controller) {
+      navigator.serviceWorker.controller.postMessage('cache-current-page');
+    }
+  }, []);
 
   const onCreateTodo = useCallback(() => {
     createTodoMutation.mutate();
