4bf89b9117c46a880b0cd3258e1dad466739fcdb
sion-k <44183313+sion-k@users.noreply.github.com>
2022-08-13 22:26:46 +0900

    fix: notification api end point update (#156)

--- services/apis/webpush.ts
+++ services/apis/webpush.ts
@@ -2,7 +2,7 @@ import fetcher from '@/shared/utils/fetcher';
 
 const webPushService = {
   notification: async (subscription: PushSubscription, id: string) =>
-    await fetcher('post', '/webpush', { subscription: JSON.stringify(subscription), id }),
+    await fetcher('post', `/webpush/${id}`, { subscription: JSON.stringify(subscription) }),
 };
 
 export default webPushService;
