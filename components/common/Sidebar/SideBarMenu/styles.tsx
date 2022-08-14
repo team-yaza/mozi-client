9d0f498cff0a492507ef6172a0c8c7506626ce95
hyunjin <leehj0110@kakao.com>
2022-08-14 15:19:08 +0900

    feat: sidebar background transition

--- components/common/Sidebar/SideBarMenu/styles.tsx
+++ components/common/Sidebar/SideBarMenu/styles.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { darkTheme, lightTheme } from '@/styles/theme';
 
 export const Container = styled.div<{ focused: boolean }>`
   width: 25rem;
@@ -8,13 +9,27 @@ export const Container = styled.div<{ focused: boolean }>`
   align-items: center;
 
   font-size: 2rem;
-
-  color: ${({ focused, theme }) => (focused ? '#735AFF' : '#585858')};
-  background-color: ${({ focused }) => (focused ? '#F4F2FF' : '#ffffff')};
-
   margin: 1rem 0rem;
   padding-left: 1.7rem;
   border-radius: 1.3rem;
+
+  color: ${({ theme, focused }) => {
+    switch (theme) {
+      case darkTheme:
+        return focused ? '#ffffff' : '#92909F';
+      case lightTheme:
+        return focused ? '#735AFF' : '#585858';
+    }
+  }};
+  background-color: ${({ focused, theme }) => {
+    switch (theme) {
+      case darkTheme:
+        return focused ? '#3B3A43' : '#2C2B30';
+      case lightTheme:
+        return focused ? '#F4F2FF' : '#ffffff';
+    }
+  }};
+  transition: 0.3s background-color;
 
   a {
     color: inherit;
