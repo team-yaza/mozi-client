249594dfdaf127ee7472f3fdb9e93e67ac23b059
hyunjin <leehj0110@kakao.com>
2022-08-19 20:14:38 +0900

    chore: add figure components

---
+++ components/common/Figure/HAMBURGER.tsx
@@ -0,0 +1,24 @@
+import { SvgWrap } from './styles';
+
+const HAMBURGER: React.FC = () => {
+  return (
+    <SvgWrap>
+      <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path
+          d="M17.5 8H2.5C2.22386 8 2 8.22386 2 8.5C2 8.77614 2.22386 9 2.5 9H17.5C17.7761 9 18 8.77614 18 8.5C18 8.22386 17.7761 8 17.5 8Z"
+          fill="#2A2A2A"
+        />
+        <path
+          d="M12.5 14H2.5C2.22386 14 2 14.2239 2 14.5C2 14.7761 2.22386 15 2.5 15H12.5C12.7761 15 13 14.7761 13 14.5C13 14.2239 12.7761 14 12.5 14Z"
+          fill="#2A2A2A"
+        />
+        <path
+          d="M19.5 20H2.5C2.22386 20 2 20.2239 2 20.5C2 20.7761 2.22386 21 2.5 21H19.5C19.7761 21 20 20.7761 20 20.5C20 20.2239 19.7761 20 19.5 20Z"
+          fill="#2A2A2A"
+        />
+      </svg>
+    </SvgWrap>
+  );
+};
+
+export default HAMBURGER;
