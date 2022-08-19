7587933cb0b5d61ec2a0224324d9b4cff12a64cb
HanCiHu <chanhui.yu.cnu@gmail.com>
2022-08-19 23:44:47 +0900

    feat: alarm 데이터 indexedDB에 저장

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,20 @@
 import { NextPage } from 'next';
 import { useCallback, useEffect, useState } from 'react';
-import { useRecoilState } from 'recoil';
 import styled from 'styled-components';
 
 import Header from '@/components/index/Header';
 import TodoList from '@/components/index/TodoList';
 import { Todo, TodoUpdateRequest } from '@/shared/types/todo';
 import todoService from '@/services/apis/todo';
-import { alarmListState } from '@/store/alarm/atom';
 import alarmService from '@/services/apis/alarm';
-import { UpdateAlarmProps } from '@/shared/types/alarm';
+import { Alarm, UpdateAlarmProps } from '@/shared/types/alarm';
 import { serializeAlarmList, serializeGeoJson } from '@/shared/utils/serialize';
+import { useLocationRef } from '@/hooks/location/useLocationRef';
+import { alarmStore } from '@/store/forage';
 
 const Home: NextPage = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
-  const [alarms, setAlarms] = useRecoilState(alarmListState);
+  const { myLocationRef, updateCurrentPosition } = useLocationRef();
 
   useEffect(() => {
     const fetchTodos = async () => {
@@ -23,37 +23,43 @@ const Home: NextPage = () => {
     };
 
     const fetchAlarms = async () => {
-      const alarmList = await alarmService.getAlarms();
-      setAlarms(serializeAlarmList(alarmList));
+      const alarmList = serializeAlarmList(await alarmService.getAlarms());
+      await alarmStore.clear();
+
+      if (!alarmList) return;
+      Promise.all(
+        alarmList.map(async (alarm: Alarm) => {
+          await alarmStore.setItem(`${alarm.todoId}`, alarm);
+        })
+      );
     };
 
     fetchAlarms();
     fetchTodos();
   }, []);
 
-  // useEffect(() => {
-  //   if ('serviceWorker' in navigator && 'SyncManager' in window) {
-  //     navigator.serviceWorker.ready.then((registration) => {
-  //       registration.sync
-  //         .register('hello-sync')
-  //         .then(() => {
-  //           return registration.sync.getTags();
-  //         })
-  //         .then((tags) => {
-  //           console.log(tags);
-  //         });
-  //     });
-  //   }
-  // }, []);
+  const sendLocation = () => {
+    if (!navigator.serviceWorker.controller) return;
+    if (!myLocationRef.current) return;
+    updateCurrentPosition();
+    navigator.serviceWorker.controller.postMessage({
+      type: 'SET_INTERVAL',
+      latitude: myLocationRef.current.longitude,
+      longitude: myLocationRef.current.longitude,
+    });
+  };
 
-  const onUpdateAlarm = useCallback(({ todoId, longitude, latitude, name }: UpdateAlarmProps) => {
-    setAlarms((prev) =>
-      prev.map((alarm) =>
-        alarm.todoId == todoId
-          ? { ...alarm, location: serializeGeoJson(longitude, latitude, name), visited: false }
-          : alarm
-      )
-    );
+  useEffect(() => {
+    const sendLocationInterval = setInterval(sendLocation, 3000);
+    return () => {
+      clearInterval(sendLocationInterval);
+    };
+  }, [myLocationRef]);
+
+  const onUpdateAlarm = useCallback(async ({ todoId, longitude, latitude, name }: UpdateAlarmProps) => {
+    const alarmTemp: Alarm | null = await alarmStore.getItem(todoId);
+    if (!alarmTemp) return;
+    alarmStore.setItem(todoId, { ...alarmTemp, location: serializeGeoJson(longitude, latitude, name) });
   }, []);
 
   const onCreateTodo = useCallback(async () => {
@@ -64,7 +70,15 @@ const Home: NextPage = () => {
 
   const onUpdateTodo = useCallback(async ({ id, title, longitude, latitude, description }: TodoUpdateRequest) => {
     setTodos((prev) =>
-      prev.map((todo) => (todo.id === id ? { ...todo, title, longitude, latitude, description } : todo))
+      prev.map((todo) => {
+        if (todo.id == id && !todo.location?.name && longitude != undefined && latitude != undefined)
+          alarmStore.setItem(id, {
+            todoId: id,
+            location: serializeGeoJson(longitude, latitude, '충남대학교'),
+            visited: false,
+          });
+        return todo.id === id ? { ...todo, title, longitude, latitude, description } : todo;
+      })
     );
 
     await todoService.updateTodo({ id, title, longitude, latitude, description });
@@ -72,6 +86,7 @@ const Home: NextPage = () => {
 
   const onDeleteTodo = useCallback(async (id: string) => {
     setTodos((prev) => prev.filter((todo) => todo.id !== id));
+    await alarmStore.removeItem(id);
 
     await todoService.deleteTodo(id);
   }, []);
--- store/forage/index.ts
+++ store/forage/index.ts
@@ -3,3 +3,7 @@ import localForage from 'localforage';
 export const todoStore = localForage.createInstance({
   name: 'todo-store',
 });
+
+export const alarmStore = localForage.createInstance({
+  name: 'alarm-store',
+});
