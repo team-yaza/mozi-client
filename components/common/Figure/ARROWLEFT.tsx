9a07480dd701d68eb428b235ef874087fb6005dd
HYUNJIN LEE <63354527+hyunjinee@users.noreply.github.com>
2022-08-21 11:27:14 +0900

    chore: fix figure props (#180)

--- components/common/Figure/ARROWLEFT.tsx
+++ components/common/Figure/ARROWLEFT.tsx
@@ -4,8 +4,8 @@ const ARROWLEFT: React.FC = () => {
   return (
     <SvgWrap>
       <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M27 8L20 15L27 22" stroke="#2A2A2A" stroke-linecap="round" stroke-linejoin="round" />
-        <path d="M20.3809 8L13.3809 15L20.3809 22" stroke="#2A2A2A" stroke-linecap="round" stroke-linejoin="round" />
+        <path d="M27 8L20 15L27 22" stroke="#2A2A2A" strokeLinecap="round" strokeLinejoin="round" />
+        <path d="M20.3809 8L13.3809 15L20.3809 22" stroke="#2A2A2A" strokeLinecap="round" strokeLinejoin="round" />
       </svg>
     </SvgWrap>
   );
