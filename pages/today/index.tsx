ed7557411e32148de8ffea060954be807884032e
hyunjin <leehj0110@kakao.com>
2022-08-23 13:42:17 +0900

    chore: 빈 페이지 생성

---
+++ pages/today/index.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from 'next';
+
+const Today: NextPage = () => {
+  return <div>today</div>;
+};
+
+export default Today;
---
+++ pages/upcoming/index.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from 'next';
+
+const Upcoming: NextPage = () => {
+  return <div>upcoming</div>;
+};
+
+export default Upcoming;
