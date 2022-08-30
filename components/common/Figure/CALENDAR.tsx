b6084d5cd4527f3c6efe37099c8749b7cd6be00e
HanCiHu <chanhui.yu.cnu@gmail.com>
2022-08-30 16:23:36 +0900

    chore: stroke-line 수정

--- components/common/Figure/CALENDAR.tsx
+++ components/common/Figure/CALENDAR.tsx
@@ -8,13 +8,13 @@ const CALENDAR: React.FC<FigureProps> = () => {
         <path
           d="M17 5H6C4.89543 5 4 5.89543 4 7V17C4 18.1046 4.89543 19 6 19H17C18.1046 19 19 18.1046 19 17V7C19 5.89543 18.1046 5 17 5Z"
           stroke="#585858"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
-        <path d="M7 9.34912H16" stroke="#585858" stroke-linecap="round" />
-        <path d="M14 15.3491H16" stroke="#585858" stroke-linecap="round" />
-        <path d="M7 4V6" stroke="#585858" stroke-linecap="round" />
-        <path d="M16 4V6" stroke="#585858" stroke-linecap="round" />
+        <path d="M7 9.34912H16" stroke="#585858" strokeLinecap="round" />
+        <path d="M14 15.3491H16" stroke="#585858" strokeLinecap="round" />
+        <path d="M7 4V6" stroke="#585858" strokeLinecap="round" />
+        <path d="M16 4V6" stroke="#585858" strokeLinecap="round" />
       </svg>
     </SvgWrap>
   );
