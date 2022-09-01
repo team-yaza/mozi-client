3da0378f8de16935558ab5b401f41c1f9ae7c751
hyunjin <leehj0110@kakao.com>
2022-09-01 12:23:41 +0900

    fix: 지도 빠르게 뜨도록 수정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
-import Script from 'next/script';
 import { useState, useCallback, Suspense } from 'react';
 import { RecoilRoot } from 'recoil';
 import { QueryClientProvider } from '@tanstack/react-query';
@@ -30,10 +29,6 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/icon.png"></link>
       </Head>
-      <Script
-        defer
-        src={`https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.NEXT_PUBLIC_NAVER_CLIENT_ID}&submodules=geocoder`}
-      ></Script>
       <GlobalStyle />
       <QueryClientProvider client={queryClient}>
         <RecoilRoot>
