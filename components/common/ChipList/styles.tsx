d1f61306faf736ddfdca763ee14a0117ff539d4e
HanCiHu <chanhui.yu.cnu@gmail.com>
2022-09-03 15:52:45 +0900

    style: 지정된 option chips 위치 style

--- components/common/ChipList/styles.tsx
+++ components/common/ChipList/styles.tsx
@@ -4,7 +4,7 @@ export const Container = styled.div<{ align: string }>`
   display: flex;
   flex-direction: ${({ align }) => align};
 
-  & > div {
+  & > div:not(:last-of-type) {
     margin: ${({ align }) => (align === 'column' ? '0 0 1rem 0' : '0 1rem 0 0')};
   }
 `;
--- components/index/TodoMap/styles.tsx
+++ components/index/TodoMap/styles.tsx
@@ -6,7 +6,7 @@ export const Container = styled.div`
 
   position: relative;
 
-  z-index: 10;
+  margin-top: 1rem;
 
   background-color: gray;
 `;
