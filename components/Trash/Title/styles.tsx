e412ed2c3d3d7ce34510b2a598f6c66191f5bc34
hyunjin <leehj0110@kakao.com>
2022-09-04 22:02:21 +0900

    feat: Title 컴포넌트

---
+++ components/Trash/Title/styles.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  position: relative;
+`;
+
+export const LogoContainer = styled.div`
+  position: relative;
+  width: 2rem;
+  height: 2rem;
+`;
+
+export const Header = styled.h2``;
--- pages/trash/index.tsx
+++ pages/trash/index.tsx
@@ -1,13 +1,26 @@
 import type { ReactElement } from 'react';
+import styled from 'styled-components';
+
 import { NextPageWithLayout } from '@/pages/_app';
 import AppLayout from '@/components/common/AppLayout';
+import Title from '@/components/Trash/Title';
 
 const Trash: NextPageWithLayout = () => {
-  return <div>today</div>;
+  return (
+    <Container>
+      <Title />
+    </Container>
+  );
 };
 
 Trash.getLayout = function getLayout(page: ReactElement) {
   return <AppLayout>{page}</AppLayout>;
 };
+
+const Container = styled.div`
+  position: relative;
+  height: 100vh;
+  width: 100%;
+`;
 
 export default Trash;
