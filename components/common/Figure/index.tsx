7a5e6909ae634b7542f5849f9b5f0d3df3dbde94
HanCiHu <chanhui.yu.cnu@gmail.com>
2022-09-05 21:29:22 +0900

    fix: figure index 수정 및 fetcher url 수정

--- components/common/Figure/index.tsx
+++ components/common/Figure/index.tsx
@@ -16,4 +16,5 @@ export { default as TAG } from './TAG';
 export { default as TODAY } from './TODAY';
 export { default as TRASH } from './TRASH';
 export { default as UPCOMING } from './UPCOMING';
+export { default as DELETE } from './DELETE';
 export { default as TODOTAG } from './TODOTAG';
