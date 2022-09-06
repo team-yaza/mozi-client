c362e1338f8ea5ea84a5a2c7f68ed9ffbe1da75a
hyunjin <leehj0110@kakao.com>
2022-09-07 04:04:47 +0900

    chore: useCookie 구현 뼈대

---
+++ hooks/useCookie.ts
@@ -0,0 +1,13 @@
+// import { useState } from 'react';
+
+// // const getItem = (key: string) => {
+// //   document.cookie.split('; ')
+// // }
+
+// export const useCookie = (key: string) => {
+//   const getCookie = () => '';
+
+//   const [cookie, setCookie] = useState(getCookie());
+
+//   const removeCookie = () => {};
+// };
