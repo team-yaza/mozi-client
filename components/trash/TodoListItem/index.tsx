4a568faf253b1c7de0623936ea40151646c8f382
hyunjin <leehj0110@kakao.com>
2022-09-14 17:13:26 +0900

    feat: Trash Page TodoListItem

--- components/trash/TodoListItem/index.tsx
+++ components/trash/TodoListItem/index.tsx
@@ -1,4 +1,14 @@
-import { Container } from './styles';
+import { useOnClickOutside } from '@/hooks/useOnClickOutside';
+import { useCallback, useEffect, useRef, useState } from 'react';
+import {
+  CheckBox,
+  Container,
+  Description,
+  DescriptionContainer,
+  MainContainer,
+  OptionsContainer,
+  Title,
+} from './styles';
 
 interface TodoListItemProps {
   title?: string;
@@ -7,11 +17,52 @@ interface TodoListItemProps {
 }
 
 const TodoListItem: React.FC<TodoListItemProps> = ({ title, description, done }) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const titleRef = useRef<HTMLDivElement>(null);
+  // const descriptionRef = useRef<HTMLDivElement>(null);
+
+  const [isFocused, setIsFoucsed] = useState(false);
+  const [isDoubleClicked, setIsDoubleClicked] = useState(false);
+
+  useEffect(() => {
+    if (titleRef && titleRef.current && title) {
+      titleRef.current.innerText = title;
+    }
+
+    description;
+  }, []);
+
+  const onClickOutsideHandler = useCallback(() => {
+    setIsFoucsed(false);
+    setIsDoubleClicked(false);
+  }, []);
+
+  useOnClickOutside(containerRef, onClickOutsideHandler);
+
+  const onClickHandler = useCallback(() => {
+    setIsFoucsed((prev) => !prev);
+  }, []);
+
+  const onDoubleClickHandler = useCallback(() => {
+    setIsFoucsed(false);
+    setIsDoubleClicked((prev) => !prev);
+  }, []);
+
   return (
-    <Container>
-      <div>{title}</div>
-      <div>{description}</div>
-      <div>{done}</div>
+    <Container ref={containerRef} isFocused={isFocused} onClick={onClickHandler} onDoubleClick={onDoubleClickHandler}>
+      <MainContainer>
+        <CheckBox checked={done} />
+        <Title ref={titleRef} contentEditable suppressContentEditableWarning />
+      </MainContainer>
+
+      {/* 더블 클릭시 생기는 부분 */}
+
+      {isDoubleClicked && (
+        <DescriptionContainer>
+          <Description />
+          <OptionsContainer>하위</OptionsContainer>
+        </DescriptionContainer>
+      )}
     </Container>
   );
 };
