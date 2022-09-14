06892218631b0e02f48f49649c57292c9d56a266
hyunjin <leehj0110@kakao.com>
2022-09-14 17:30:35 +0900

    feat: add Loading Spinner to trash page

--- pages/trash/index.tsx
+++ pages/trash/index.tsx
@@ -2,16 +2,25 @@ import type { ReactElement } from 'react';
 import styled from 'styled-components';
 
 import { NextPageWithLayout } from '@/pages/_app';
+import { useTodoListQuery } from '@/hooks/apis/todo/useTodoListQuery';
 import AppLayout from '@/components/common/AppLayout';
 import Title from '@/components/trash/Title';
 import TodoList from '@/components/trash/TodoList';
-import { useTodoListQuery } from '@/hooks/apis/todo/useTodoListQuery';
+import Spinner from '@/components/common/Spinner';
 
 const Trash: NextPageWithLayout = () => {
-  const { data: todos } = useTodoListQuery();
+  const { data: todos, isLoading } = useTodoListQuery();
+
   return (
     <Container>
       <Title />
+
+      {isLoading && (
+        <SpinnerContainer>
+          <Spinner />
+        </SpinnerContainer>
+      )}
+
       <TodoList todos={todos} />
     </Container>
   );
@@ -25,6 +34,14 @@ const Container = styled.div`
   position: relative;
   height: 100vh;
   width: 100%;
+`;
+
+const SpinnerContainer = styled.div`
+  position: absolute;
+  top: 0;
+
+  width: 100%;
+  height: 100%;
 `;
 
 export default Trash;
