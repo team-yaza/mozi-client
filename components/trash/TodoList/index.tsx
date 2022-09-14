956dfb1d48e4ef0bae0d7e0f3496bf12b689d5b3
hyunjin <leehj0110@kakao.com>
2022-09-14 14:04:42 +0900

    feat: Trash 페이지에서 TodoList 컴포넌트 껍데기 제작

---
+++ components/trash/TodoList/index.tsx
@@ -0,0 +1,26 @@
+import TodoListItem from '@/components/trash/TodoListItem';
+import { Todo } from '@/shared/types/todo';
+import React, { useEffect } from 'react';
+import { Container } from './styles';
+
+interface TodoListProps {
+  todos: Todo[];
+}
+
+const TodoList: React.FC<TodoListProps> = ({ todos }) => {
+  console.log('hi');
+
+  useEffect(() => {
+    console.log(todos);
+  }, [todos]);
+
+  return (
+    <Container>
+      {todos.map((todo) => (
+        <TodoListItem key={todo.id} title={todo.title} description={todo.description} done={todo.done} />
+      ))}
+    </Container>
+  );
+};
+
+export default TodoList;
