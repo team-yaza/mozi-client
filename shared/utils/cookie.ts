4ecf82b51f3410c376785611b4f573726ee3e525
hyunjin <leehj0110@kakao.com>
2022-09-15 19:55:16 +0900

    fix: getCookie 함수에 window 조건 추가

--- shared/utils/cookie.ts
+++ shared/utils/cookie.ts
@@ -1,10 +1,12 @@
 export const getCookie = (name: string) => {
-  const cookieArr = document.cookie.split(';');
+  if (typeof window !== 'undefined') {
+    const cookieArr = document.cookie.split(';');
 
-  for (let i = 0; i < cookieArr.length; i++) {
-    const cookiePair = cookieArr[i].split('=');
-    if (name == cookiePair[0].trim()) {
-      return decodeURIComponent(cookiePair[1]);
+    for (let i = 0; i < cookieArr.length; i++) {
+      const cookiePair = cookieArr[i].split('=');
+      if (name == cookiePair[0].trim()) {
+        return decodeURIComponent(cookiePair[1]);
+      }
     }
   }
   return null;
