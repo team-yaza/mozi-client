87406f6ef4db78a366a3cbd31ceb7b7f21a63e9f
hyunjin <leehj0110@kakao.com>
2022-09-15 19:54:32 +0900

    feat: add headers to axios apiClient

--- shared/utils/fetcher.ts
+++ shared/utils/fetcher.ts
@@ -1,13 +1,17 @@
 import axios from 'axios';
+import { getCookie } from './cookie';
 
-const api = axios.create({
+const apiClient = axios.create({
   baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:3001/api/v1' : 'https://mozi-server.com/api/v1',
   withCredentials: true,
+  headers: {
+    Authorization: `Bearer ${getCookie('token')}`,
+  },
 });
 
 const fetcher = async (method: 'get' | 'post' | 'patch' | 'delete', url: string, ...rest: object[]) => {
   try {
-    const { data } = await api[method](url, ...rest);
+    const { data } = await apiClient[method](url, ...rest);
 
     return data;
   } catch (error) {
