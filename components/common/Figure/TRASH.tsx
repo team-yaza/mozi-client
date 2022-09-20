bfb6b606441f130c439e0531adc51271da2aa93b
hyunjin <leehj0110@kakao.com>
2022-09-20 13:09:12 +0900

    chore: Trash svg fill 속성 수정

--- components/common/Figure/TRASH.tsx
+++ components/common/Figure/TRASH.tsx
@@ -7,7 +7,6 @@ const TRASH: React.FC<FigureProps> = ({ focused }) => {
       <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path
           d="M4.5 4H16.5V15C16.5 15.7956 16.1839 16.5587 15.6213 17.1213C15.0587 17.6839 14.2956 18 13.5 18H7.5C6.70435 18 5.94129 17.6839 5.37868 17.1213C4.81607 16.5587 4.5 15.7956 4.5 15V4Z"
-          fill="white"
           stroke={focused ? '#735AFF' : '#585858'}
           strokeLinecap="round"
           strokeLinejoin="round"
