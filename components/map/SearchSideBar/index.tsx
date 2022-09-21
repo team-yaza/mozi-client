4fd567f13a3f4847cc7dbdb414a19edb35d99a56
hyunjin <leehj0110@kakao.com>
2022-09-21 21:20:40 +0900

    feat: 검색하면 최근 검색에 저장 및 로컬 스토리지에 저장

--- components/map/SearchSideBar/index.tsx
+++ components/map/SearchSideBar/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useCallback, Dispatch, SetStateAction, useRef } from 'react';
+import { useState, useCallback, Dispatch, SetStateAction, useRef, useEffect } from 'react';
 import { useRecoilState } from 'recoil';
 
 import RecentSearch from './RecentSearch';
@@ -6,6 +6,9 @@ import { searchSideBarAtom } from '@/store/sidebar/atom';
 import { getLocationSearchResult } from '@/shared/utils/map';
 import { getCurrentPosition } from '@/shared/utils/location';
 import { SIDEBARARROWLEFT } from '@/components/common/Figure';
+import { Location, LocationSearchResult } from '@/shared/types/location';
+import { useOnClickOutside } from '@/hooks/useOnClickOutside';
+import { getItem, setItem } from '@/store/localStorage';
 import {
   Container,
   SearchContainer,
@@ -17,13 +20,6 @@ import {
   SearchResultItem,
   SearchResultHeading,
 } from './styles';
-import { Location } from '@/shared/types/location';
-import { useOnClickOutside } from '@/hooks/useOnClickOutside';
-
-interface SearchResult {
-  name: string;
-  location: [latitude: number, longitude: number];
-}
 
 interface SearchSideBarProps {
   // naverMap: naver.maps.Map | undefined;
@@ -35,14 +31,23 @@ interface SearchSideBarProps {
 const SearchSideBar: React.FC<SearchSideBarProps> = ({ setCoords }) => {
   const [keyword, setKeyword] = useState('');
   const [isSearching, setIsSearching] = useState(false);
-  const [searchResult, setSearchResult] = useState<SearchResult[]>([]);
   const [isSearchBarOpen, setIsSearchBarOpen] = useRecoilState(searchSideBarAtom);
+  const [searchResult, setSearchResult] = useState<LocationSearchResult[]>([]);
+  const [recentSearch, setRecentSearch] = useState<LocationSearchResult[]>([]);
 
   const searchResultContainerRef = useRef<HTMLDivElement>(null);
 
   useOnClickOutside(searchResultContainerRef, () => {
     clearSearch();
   });
+
+  useEffect(() => {
+    const recentSearchFromLocalStorage = getItem('RECENT_SEARCH');
+
+    if (recentSearchFromLocalStorage) {
+      setRecentSearch(JSON.parse(recentSearchFromLocalStorage));
+    }
+  }, []);
 
   const clearSearch = useCallback(() => {
     setIsSearching(false);
@@ -82,28 +87,40 @@ const SearchSideBar: React.FC<SearchSideBarProps> = ({ setCoords }) => {
     [setCoords]
   );
 
+  const onChangeRecentSearchResult = (result: LocationSearchResult) => {
+    const hasSameRecentSearch = recentSearch.some((recentSearchItem) => recentSearchItem.name === result.name);
+
+    if (hasSameRecentSearch) return;
+
+    setRecentSearch([result, ...recentSearch].slice(0, 5));
+    setItem('RECENT_SEARCH', JSON.stringify([result, ...recentSearch].slice(0, 5)));
+  };
+
   return (
     <Container isSearchBarOpen={isSearchBarOpen}>
       <SearchContainer isSeraching={isSearching}>
         <SearchInput value={keyword} onChange={onChange} onKeyDown={onKeyDown} placeholder="검색어를 입력하세요." />
-        <SearchResultContainer ref={searchResultContainerRef} isSeraching={isSearching}>
-          <SearchResultHeading>장소</SearchResultHeading>
-          {isSearching && (
+        {isSearching && (
+          <SearchResultContainer ref={searchResultContainerRef} isSeraching={isSearching}>
+            <SearchResultHeading>장소</SearchResultHeading>
             <SearchResultList>
               {searchResult.map((result, index) => (
                 <SearchResultItem
                   key={index}
-                  onClick={() => onChangePosition({ longitude: result.location[0], latitude: result.location[1] })}
+                  onClick={() => {
+                    onChangePosition({ longitude: result.location[0], latitude: result.location[1] });
+                    onChangeRecentSearchResult(result);
+                  }}
                 >
                   {result.name}
                 </SearchResultItem>
               ))}
             </SearchResultList>
-          )}
-        </SearchResultContainer>
+          </SearchResultContainer>
+        )}
       </SearchContainer>
 
-      <RecentSearch />
+      <RecentSearch recentSearch={recentSearch} />
 
       <SideBarToggleButton type="button" onClick={() => setIsSearchBarOpen(!isSearchBarOpen)}>
         {isSearchBarOpen ? '닫기' : '열기'}
