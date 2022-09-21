a26d1f4182767aba4ce252bfb4147428e5e1ff28
hyunjin <leehj0110@kakao.com>
2022-09-21 18:26:35 +0900

    feat: 검색 리스트 바깥을 클릭시 검색화면 꺼지는 기능

--- components/map/SearchSideBar/index.tsx
+++ components/map/SearchSideBar/index.tsx
@@ -1,8 +1,8 @@
-import { useState, useCallback, Dispatch, SetStateAction } from 'react';
+import { useState, useCallback, Dispatch, SetStateAction, useRef } from 'react';
 import { useRecoilState } from 'recoil';
 
 import RecentSearch from './RecentSearch';
-import { sideBarStateAtom } from '@/store/sidebar/atom';
+import { searchSideBarAtom } from '@/store/sidebar/atom';
 import { getLocationSearchResult } from '@/shared/utils/map';
 import { getCurrentPosition } from '@/shared/utils/location';
 import { SIDEBARARROWLEFT } from '@/components/common/Figure';
@@ -18,6 +18,7 @@ import {
   SearchResultHeading,
 } from './styles';
 import { Location } from '@/shared/types/location';
+import { useOnClickOutside } from '@/hooks/useOnClickOutside';
 
 interface SearchResult {
   name: string;
@@ -34,8 +35,19 @@ interface SearchSideBarProps {
 const SearchSideBar: React.FC<SearchSideBarProps> = ({ setCoords }) => {
   const [keyword, setKeyword] = useState('');
   const [isSearching, setIsSearching] = useState(false);
-  const [searchResult, setSearchResult] = useState<Array<SearchResult>>([]);
-  const [isSearchBarOpen, setIsSearchBarOpen] = useRecoilState(sideBarStateAtom);
+  const [searchResult, setSearchResult] = useState<SearchResult[]>([]);
+  const [isSearchBarOpen, setIsSearchBarOpen] = useRecoilState(searchSideBarAtom);
+
+  const searchResultContainerRef = useRef<HTMLDivElement>(null);
+
+  useOnClickOutside(searchResultContainerRef, () => {
+    clearSearch();
+  });
+
+  const clearSearch = useCallback(() => {
+    setIsSearching(false);
+    setSearchResult([]);
+  }, []);
 
   const onKeyDown = useCallback((e: React.KeyboardEvent) => {
     e.stopPropagation();
@@ -43,6 +55,11 @@ const SearchSideBar: React.FC<SearchSideBarProps> = ({ setCoords }) => {
 
   const onChange = useCallback(async (e: React.ChangeEvent<HTMLInputElement>) => {
     setKeyword(e.target.value);
+
+    if (e.target.value === '') {
+      clearSearch();
+      return;
+    }
 
     const { coords } = await getCurrentPosition();
 
@@ -52,11 +69,9 @@ const SearchSideBar: React.FC<SearchSideBarProps> = ({ setCoords }) => {
       longitude: coords.longitude,
     });
 
-    if (result.length > 0) {
+    if (e.target.value.length > 0) {
       setIsSearching(true);
       setSearchResult(result);
-
-      console.log(result);
     }
   }, []);
 
@@ -71,18 +86,20 @@ const SearchSideBar: React.FC<SearchSideBarProps> = ({ setCoords }) => {
     <Container isSearchBarOpen={isSearchBarOpen}>
       <SearchContainer isSeraching={isSearching}>
         <SearchInput value={keyword} onChange={onChange} onKeyDown={onKeyDown} placeholder="검색어를 입력하세요." />
-        <SearchResultContainer isSeraching={isSearching}>
+        <SearchResultContainer ref={searchResultContainerRef} isSeraching={isSearching}>
           <SearchResultHeading>장소</SearchResultHeading>
-          <SearchResultList>
-            {searchResult.map((result, index) => (
-              <SearchResultItem
-                key={index}
-                onClick={() => onChangePosition({ longitude: result.location[0], latitude: result.location[1] })}
-              >
-                {result.name}
-              </SearchResultItem>
-            ))}
-          </SearchResultList>
+          {isSearching && (
+            <SearchResultList>
+              {searchResult.map((result, index) => (
+                <SearchResultItem
+                  key={index}
+                  onClick={() => onChangePosition({ longitude: result.location[0], latitude: result.location[1] })}
+                >
+                  {result.name}
+                </SearchResultItem>
+              ))}
+            </SearchResultList>
+          )}
         </SearchResultContainer>
       </SearchContainer>
 
