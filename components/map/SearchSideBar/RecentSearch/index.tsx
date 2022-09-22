93bb401503961a0e8f132fe517bc97f5d7a2749b
hyunjin <leehj0110@kakao.com>
2022-09-22 11:16:12 +0900

    feat: 최근 검색어를 클릭해도 지도에서 이동할 수 있다.

--- components/map/SearchSideBar/RecentSearch/index.tsx
+++ components/map/SearchSideBar/RecentSearch/index.tsx
@@ -1,17 +1,28 @@
-import { LocationSearchResult } from '@/shared/types/location';
+import { Dispatch, SetStateAction } from 'react';
+import { Location, LocationSearchResult } from '@/shared/types/location';
 import { Container, RecentSearchHeading, RecentSearchKeyword, RecentSearchList } from './styles';
 
 interface RecentSearchProps {
   recentSearch: LocationSearchResult[];
+  setCoords: Dispatch<SetStateAction<Location | undefined>>;
 }
 
-const RecentSearch: React.FC<RecentSearchProps> = ({ recentSearch }) => {
+const RecentSearch: React.FC<RecentSearchProps> = ({ recentSearch, setCoords }) => {
   return (
     <Container>
       <RecentSearchHeading>최근 검색어</RecentSearchHeading>
       <RecentSearchList>
         {recentSearch.map((recentSearchItem) => (
-          <RecentSearchKeyword>{recentSearchItem.name}</RecentSearchKeyword>
+          <RecentSearchKeyword
+            onClick={() =>
+              setCoords({
+                longitude: recentSearchItem.location[0],
+                latitude: recentSearchItem.location[1],
+              })
+            }
+          >
+            {recentSearchItem.name}
+          </RecentSearchKeyword>
         ))}
       </RecentSearchList>
     </Container>
