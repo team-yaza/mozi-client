34166729512a52ed84883af7100b7a636b6ef9c9
hyunjin <leehj0110@kakao.com>
2022-09-26 20:22:31 +0900

    feat: 사용자는 로그북을 사용하여 완료된 Todo를 확인할 수 있다.

--- src/pages/logbook/index.tsx
+++ src/pages/logbook/index.tsx
@@ -1,16 +1,35 @@
-import styled from 'styled-components';
-import { NextPageWithLayout } from '@/pages/_app';
 import { ReactElement } from 'react';
+import styled from 'styled-components';
+
+import { NextPageWithLayout } from '@/pages/_app';
 import AppLayout from '@/components/common/AppLayout/index';
+import Title from '@/components/logbook/Title';
+import TodoList from '@/components/common/TodoList/index';
+import { useLogbookTodoList } from '@/hooks/apis/todo/useTodoListQuery';
+import { useDeleteTodoMutation, useUpdateTodoMutation } from '@/hooks/apis/todo/useTodoMutation';
 
 const Logbook: NextPageWithLayout = () => {
-  return <Container>hi</Container>;
+  const { data: todos } = useLogbookTodoList();
+  const { mutate: updateTodo } = useUpdateTodoMutation();
+  const { mutate: deleteTodo } = useDeleteTodoMutation();
+
+  return (
+    <Container>
+      <Title />
+
+      <TodoList todos={todos} updateTodo={updateTodo} deleteTodo={deleteTodo} />
+    </Container>
+  );
 };
 
 Logbook.getLayout = function getLayout(page: ReactElement) {
   return <AppLayout>{page}</AppLayout>;
 };
 
-const Container = styled.div``;
+const Container = styled.div`
+  position: relative;
+  height: 100vh;
+  width: 100%;
+`;
 
 export default Logbook;
