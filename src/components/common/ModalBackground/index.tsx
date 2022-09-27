9230f66fe36e4536b1ed6b0d0b34504c64278f51
HanCiHu <chanhui.yu.cnu@gmail.com>
2022-09-27 20:30:51 +0900

    feat: modal background 컴포넌트 구현

---
+++ src/components/common/ModalBackground/index.tsx
@@ -0,0 +1,7 @@
+import { Container } from './styles';
+
+const ModalBackground: React.FC = () => {
+  return <Container></Container>;
+};
+
+export default ModalBackground;
---
+++ src/components/common/ModalBackground/styles.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  position: absolute;
+  top: 0;
+  background-color: #585858;
+  opacity: 0.4;
+  z-index: 101;
+  width: 100%;
+  height: 100%;
+`;
