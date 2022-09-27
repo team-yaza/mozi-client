31820d67480551202c077bfd00f74cd269a66a64
HanCiHu <chanhui.yu.cnu@gmail.com>
2022-09-27 15:18:15 +0900

    fix: 사용하지 않는 GeoJson Type 삭제

--- src/shared/types/location.ts
+++ src/shared/types/location.ts
@@ -2,13 +2,6 @@ export interface Location {
   latitude: number;
   longitude: number;
 }
-
-export interface GeoJson {
-  type: string;
-  name: string;
-  coordinates: Array<number>;
-}
-
 export interface LocationSearchResult {
   name: string;
   location: [latitude: number, longitude: number];
