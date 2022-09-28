e34bfe037b6b139bdbea45b9d754a79b3d13ad05
HYUNJIN LEE <63354527+hyunjinee@users.noreply.github.com>
2022-09-28 09:16:48 +0900

    Update src/components/common/SetLocationModal/index.tsx
    Co-authored-by: codacy-production[bot] <61871480+codacy-production[bot]@users.noreply.github.com>

--- src/components/common/SetLocationModal/index.tsx
+++ src/components/common/SetLocationModal/index.tsx
@@ -9,7 +9,7 @@ interface SetLocationModalProps {
   longitude?: number;
   latitude?: number;
   setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
-  updateTodo: UseMutateFunction<any, unknown, TodoUpdateRequest, unknown>;
+  updateTodo: UseMutateFunction<unknown, unknown, TodoUpdateRequest, unknown>;
 }
 
 const SetLocationModal: React.FC<SetLocationModalProps> = ({ id, longitude, latitude, updateTodo, setIsModalOpen }) => {
