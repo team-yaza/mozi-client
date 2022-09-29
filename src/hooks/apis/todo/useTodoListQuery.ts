a5826397446213c651af1209de50c08fe3b077e3
HanCiHu <chanhui.yu.cnu@gmail.com>
2022-09-29 18:40:02 +0900

    fix: LogbookTodo 삭제된 Todo가 있던 버그 수정

--- src/hooks/apis/todo/useTodoListQuery.ts
+++ src/hooks/apis/todo/useTodoListQuery.ts
@@ -29,7 +29,7 @@ export const useSoftDeletedTodoList = () => {
 
 export const useLogbookTodoList = () => {
   return useQuery(['todos'], todoService.getTodos, {
-    select: useCallback((todos: Todo[]) => todos.filter((todo) => todo.done), []),
+    select: useCallback((todos: Todo[]) => todos.filter((todo) => todo.done && !todo.deletedAt), []),
     onSuccess: (data: any) => {
       console.log(data, ' 먼데');
     },
