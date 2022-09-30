4a414aff3748ced30ac240a2bdb949cad030963d
hyunjin <leehj0110@kakao.com>
2022-09-30 13:43:56 +0900

    feat: add fade in/out animations

---
+++ src/styles/animation.ts
@@ -0,0 +1,19 @@
+import { keyframes } from 'styled-components';
+
+export const fadeIn = keyframes`
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+`;
+
+export const fadeOut = keyframes`
+  0% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
+`;
