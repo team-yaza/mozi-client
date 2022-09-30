66f650d178aea0716307f296f8bf12809a6c5255
hyunjin <leehj0110@kakao.com>
2022-09-30 22:43:27 +0900

    chore: 기존 모달 제거

--- src/components/common/TodoListItem/Map/index.tsx
+++ src/components/common/TodoListItem/Map/index.tsx
@@ -1,13 +1,12 @@
-import React, { useRef, useCallback, useState, useEffect } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { UseMutateFunction } from '@tanstack/react-query';
 
 import { useNaverMap } from '@/hooks/useNaverMap';
 import { TodoUpdateRequest } from '@/shared/types/todo';
 import { CONFIRMBUTTON } from '@/components/common/Figure';
 import Spinner from '@/components/common/Spinner';
-import Portal from '@/components/common/Portal';
-import ModalBackground from '@/components/common/ModalBackground';
-import SetLocationModal from '@/components/common/SetLocationModal';
+import Modal from '@/components/common/Modal';
+import SetLocationModal from '@/components/map/SetLocationModal';
 import { Container, SpinnerContainer, ConfirmContainer } from './styles';
 
 interface MapProps {
@@ -18,53 +17,49 @@ interface MapProps {
   setIsMapOpened: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const Map = ({ id, longitude, latitude, updateTodo, setIsMapOpened }: MapProps) => {
+const Map = ({ longitude, latitude }: MapProps) => {
   const naverMapRef = useRef<HTMLDivElement>(null);
-  const { isMapLoading, markerCoords, setCoords } = useNaverMap();
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const { isMapLoading, setCoords } = useNaverMap();
+  const [isModalOpened, setIsModalOpened] = useState(false);
 
   useEffect(() => {
-    if (longitude && latitude) {
-      setCoords({ longitude, latitude });
-    }
+    if (longitude && latitude) setCoords({ longitude, latitude });
   }, [longitude, latitude]);
 
-  const updateLocationHandler = useCallback(() => {
-    setIsModalOpen((prev) => !prev);
-  }, [markerCoords]);
-
   return (
-    <Container>
-      {isMapLoading && (
-        <SpinnerContainer>
-          <Spinner />
-        </SpinnerContainer>
-      )}
-      {isModalOpen && (
-        <Portal>
-          <SetLocationModal
-            id={id}
-            updateTodo={updateTodo}
-            setIsMapOpened={setIsMapOpened}
-            setIsModalOpen={setIsModalOpen}
-            longitude={markerCoords?.longitude}
-            latitude={markerCoords?.latitude}
-          />
-          <ModalBackground setIsModalOpen={setIsModalOpen} />
-        </Portal>
-      )}
+    <>
+      <Container>
+        {isMapLoading && (
+          <SpinnerContainer>
+            <Spinner />
+          </SpinnerContainer>
+        )}
 
-      {/* 네이버 지도 */}
-      <div id="map" ref={naverMapRef} style={{ width: '100%', height: '30rem' }}></div>
+        {/* 네이버 지도 */}
+        <div id="map" ref={naverMapRef} style={{ width: '100%', height: '30rem' }}></div>
 
-      {/* 위치 선택 확인 버튼 */}
-      {!isMapLoading && (
-        <ConfirmContainer onClick={updateLocationHandler}>
-          <CONFIRMBUTTON />
-        </ConfirmContainer>
-      )}
-    </Container>
+        {/* 위치 선택 확인 버튼 */}
+        {!isMapLoading && (
+          <ConfirmContainer onClick={() => setIsModalOpened(true)}>
+            <CONFIRMBUTTON />
+          </ConfirmContainer>
+        )}
+
+        {/* 모달  */}
+        <Modal
+          isOpened={isModalOpened}
+          onConfirm={function (): void {
+            throw new Error('Function not implemented.');
+          }}
+          onClose={() => setIsModalOpened(false)}
+        >
+          ddd
+        </Modal>
+
+        <SetLocationModal />
+      </Container>
+    </>
   );
 };
 
-export default Map;
+export default React.memo(Map);
