3c4d6d00d4c11427395ccebb43fb4a3af9ffff84
hyunjin <leehj0110@kakao.com>
2022-09-30 17:05:23 +0900

    style: theme 색깔 및 폰트 추가

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -7,13 +7,73 @@ export const theme = {
     main: '#775EFF',
     black: '#000000',
     grey: '#E9E9E9',
+    grey1: '#1f1f1f',
+    grey2: '#272727',
+    grey3: '#3a3a3b',
+    grey4: '#7f7f7f',
+    grey5: '#a6a6a6',
+    grey6: '#bfbfbf',
     darkGrey: '#92909F',
     lightGrey: '#FEFEFE',
     kakao: '#FEE502',
   },
+  fonts: {
+    h1: `
+    font-size: 2.4rem;
+    font-weight: bold;
+  `,
+    h2: `
+    font-size: 2rem;
+    font-weight: bold;
+  `,
+    h3: `
+    font-size: 1.8rem;
+    font-weight: bold;
+  `,
+    h4: `
+    font-size: 1.6rem;
+    font-weight: 500;
+  `,
+    h5: `
+    font-size: 1.4rem;
+    font-weight: bold;
+  `,
+    h6: `
+    font-size: 1.4rem;
+    font-weight: 500;
+  `,
+    subtitle1: `
+    font-size: 2.4rem;
+    font-weight: 500;
+    line-height: 144%;
+  `,
+    body: `
+    font-size: 1.4rem;
+    font-weight: 500;
+    line-height: 170%;
+  `,
+    caption1: `
+    font-size: 1.2rem;
+    font-weight: 500;
+  `,
+    caption2: `
+    font-size: 1.2rem;
+    font-weight: bold;
+  `,
+    btn1: `
+    font-size: 1.6rem;
+    font-weight: bold;
+    line-height: 1.8rem;
+  `,
+    btn2: `
+    font-size: 1.4rem;
+    font-weight: 500;
+    line-height: 1.6rem;
+  `,
+  },
 };
 
-export const lightTheme: DefaultTheme = {
+export const lightTheme: Omit<DefaultTheme, 'fonts'> = {
   color: {
     text: theme.colors.black,
     black: '#ffffff',
@@ -33,7 +93,7 @@ export const lightTheme: DefaultTheme = {
   },
 };
 
-export const darkTheme: DefaultTheme = {
+export const darkTheme: Omit<DefaultTheme, 'fonts'> = {
   color: {
     text: theme.colors.white,
     black: '#000000',
