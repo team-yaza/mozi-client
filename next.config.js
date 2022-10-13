3061d41ef6f96230f5b2851dd914a67e2d06e528
HYUNJIN LEE <63354527+hyunjinee@users.noreply.github.com>
2022-10-13 23:44:51 +0900

    fix: service worker registration by sentry (#339)

--- next.config.js
+++ next.config.js
@@ -8,15 +8,7 @@ const nextConfig = {
 };
 
 const moduleExports = {
-  // Your existing module.exports
-
   sentry: {
-    // Use `hidden-source-map` rather than `source-map` as the Webpack `devtool`
-    // for client-side builds. (This will be the default starting in
-    // `@sentry/nextjs` version 8.0.0.) See
-    // https://webpack.js.org/configuration/devtool/ and
-    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/#use-hidden-source-map
-    // for more information.
     hideSourceMaps: true,
   },
 };
@@ -33,8 +25,6 @@ const sentryWebpackPluginOptions = {
 };
 
 module.exports = withSentryConfig(
-  moduleExports,
-  sentryWebpackPluginOptions,
   withPlugins([
     withPWA({
       pwa: {
@@ -43,16 +33,7 @@ module.exports = withSentryConfig(
       },
     }),
     nextConfig,
-  ])
+  ]),
+  moduleExports,
+  sentryWebpackPluginOptions
 );
-
-// // This file sets a custom webpack configuration to use your Next.js app
-// // with Sentry.
-// // https://nextjs.org/docs/api-reference/next.config.js/introduction
-// // https://docs.sentry.io/platforms/javascript/guides/nextjs/
-
-// const { withSentryConfig } = require('@sentry/nextjs');
-
-// // Make sure adding Sentry options is the last code to run before exporting, to
-// // ensure that your source maps include changes from all other Webpack plugins
-// module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
