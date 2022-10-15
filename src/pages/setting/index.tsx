67c305f439ff0e949893e76e47da718b11cfa68a
hyunjin <leehj0110@kakao.com>
2022-10-15 11:56:32 +0900

    feat: 로그아웃

--- src/pages/setting/index.tsx
+++ src/pages/setting/index.tsx
@@ -1,16 +1,23 @@
 import { ReactElement } from 'react';
 import styled from 'styled-components';
+// import { useRouter } from 'next/router';
 
 import { NextPageWithLayout } from '@/pages/_app';
 import AppLayout from '@/components/common/AppLayout';
 import Title from '@/components/setting/Title';
 import Theme from '@/components/setting/Theme';
+// import Button from '@/components/common/Button/index';
 
 const Setting: NextPageWithLayout<{ setTheme: () => void }> = ({ setTheme }) => {
+  // const router = useRouter();
+
   return (
     <Container>
       <Title />
       <Theme setTheme={setTheme} />
+      {/* <LogoutButton color="primary" size="large">
+        로그아웃
+      </LogoutButton> */}
     </Container>
   );
 };
@@ -22,5 +29,7 @@ const Container = styled.div`
   height: 100vh;
   width: 100%;
 `;
+
+// const LogoutButton = styled(Button)``;
 
 export default Setting;
