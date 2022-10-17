3758e1bcf33448b4d5acc8f263d10eeb8c17dda8
hyunjin <leehj0110@kakao.com>
2022-10-17 13:54:26 +0900

    refactor: sidebar에서 상위 컴포넌트로 data fetching 로직 이동

--- src/components/common/AppLayout/index.tsx
+++ src/components/common/AppLayout/index.tsx
@@ -1,14 +1,17 @@
 import { Container } from './styles';
 import Sidebar from '@/components/common/Sidebar';
+import { useTodoListStatistics } from '@/hooks/apis/todo/useTodoListQuery';
 
 interface AppLayoutProps {
   children: React.ReactNode;
 }
 
 const AppLayout: React.FC<AppLayoutProps> = ({ children }) => {
+  const { data: statistics } = useTodoListStatistics();
+
   return (
     <Container>
-      <Sidebar />
+      <Sidebar statistics={statistics} />
       {children}
     </Container>
   );
