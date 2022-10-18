bd8d222ae4eb558d288ec95943885df5335bf98c
HYUNJIN LEE <63354527+hyunjinee@users.noreply.github.com>
2022-10-18 16:32:09 +0900

    feat: Profile 공통 컴포넌트 (#364)
    * fix: stroybook에서 이미지 안뜨는 문제 해결
    
    * feat: Profile 컴포넌트, storybook
    
    * style: sidebar style margin 수정

---
+++ src/components/common/Profile/styles.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  position: relative;
+  align-self: flex-start;
+  overflow: hidden;
+`;
+
+export const LogoContainer = styled.div`
+  position: relative;
+  width: 4.4rem;
+  height: 4.4rem;
+`;
