75ae5f07493744249723bba8ae6827925478cbc8
hyunjin <leehj0110@kakao.com>
2022-10-20 17:15:53 +0900

    feat: syncTodos 함수

--- src/shared/utils/sync.ts
+++ src/shared/utils/sync.ts
@@ -1,35 +1,35 @@
-import { SYNC_TODOS } from '@/shared/constants/sync';
-import { getCookie } from './cookie';
+import { isBackgroundSyncAvailable, sendAuthTokenToServiceWorker } from './serviceWorker';
+import { SYNC_TODOS } from '@/shared/constants/serviceWorker';
 
 export const syncTodos = async () => {
   if ('serviceWorker' in navigator && 'SyncManager' in window) {
-    try {
-      // 권한을 확인하는 코드 -> 설치된 PWA에서만 sync 권한이 부여된다.
-      // navigator.permissions.query({ name: 'periodic-background-sync' }).then((status) => {
-      //   console.log(status);
-      // });
+    sendAuthTokenToServiceWorker(); // 서비스워커에서 요청을 보내려면 토큰이 필요
 
-      const registration: ServiceWorkerRegistration = await navigator.serviceWorker.ready;
+    const registration = await navigator.serviceWorker.ready;
+    const isSyncAvailable = await isBackgroundSyncAvailable();
 
-      // registration.pushManager.subscribe
-
-      // navigator.serviceWorker.controller.postMessage({
-      //   type: 'SET_INTERVAL',
-      //   latitude: myLocationRef.current.latitude,
-      //   longitude: myLocationRef.current.longitude,
-      // });
-
-      navigator.serviceWorker.controller?.postMessage({
-        type: 'TOKEN',
-        token: getCookie('token'),
-      });
-
+    if (isSyncAvailable) {
       await registration.sync.register(SYNC_TODOS);
-      const tags = await registration.sync.getTags();
-      console.log(tags);
-    } catch (e) {
-      console.log(e);
-      console.log('싱크이벤트 등록 에러');
+    } else {
+      console.log('데이터베이스 동기화 요청에 실패했습니다.');
     }
+    // try {
+    //   const registration: ServiceWorkerRegistration = await navigator.serviceWorker.ready;
+
+    //   // registration.pushManager.subscribe
+
+    //   // navigator.serviceWorker.controller.postMessage({
+    //   //   type: 'SET_INTERVAL',
+    //   //   latitude: myLocationRef.current.latitude,
+    //   //   longitude: myLocationRef.current.longitude,
+    //   // });
+
+    //   await registration.sync.register(SYNC_TODOS);
+    //   const tags = await registration.sync.getTags();
+    //   console.log(tags);
+    // } catch (e) {
+    //   console.log(e);
+    //   console.log('싱크이벤트 등록 에러');
+    // }
   }
 };
