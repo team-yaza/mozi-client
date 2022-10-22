1f30351e0f94a38bd913b1716bc479b1186a99f0
hyunjin <leehj0110@kakao.com>
2022-10-22 19:08:02 +0900

    feat: findMaximumIndexAtTodoStore 함수 작성

--- src/store/localForage/index.ts
+++ src/store/localForage/index.ts
@@ -3,3 +3,15 @@ import localForage from 'localforage';
 export const todoStore = localForage.createInstance({
   name: 'todo-store',
 });
+
+export const findMaximumIndexAtTodoStore = async () => {
+  const keys = await todoStore.keys();
+
+  if (keys.length === 0) {
+    return 0;
+  }
+
+  const todos = await Promise.all(keys.map((key) => todoStore.getItem(key)));
+
+  return Math.max(...todos.map((todo: any) => todo.index));
+};
