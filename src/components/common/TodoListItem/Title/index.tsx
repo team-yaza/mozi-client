8166b46aa2cf467b16b38a35e414f5f8a24a5372
hyunjin <leehj0110@kakao.com>
2022-10-24 20:03:39 +0900

    refactor: TodoListItemTitle

--- src/components/common/TodoListItem/Title/index.tsx
+++ src/components/common/TodoListItem/Title/index.tsx
@@ -8,7 +8,7 @@ interface TitleProps {
   id: string;
   title?: string;
   isDoubleClicked: boolean;
-  updateTodo: UseMutateFunction<any, unknown, TodoUpdateRequest, unknown>;
+  updateTodo: UseMutateFunction<unknown, unknown, TodoUpdateRequest, unknown>;
 }
 
 const Title: React.FC<TitleProps> = ({ id, title = '', isDoubleClicked, updateTodo }) => {
@@ -18,17 +18,21 @@ const Title: React.FC<TitleProps> = ({ id, title = '', isDoubleClicked, updateTo
     if (titleRef.current && title) {
       titleRef.current.innerText = title;
     }
+    // 의존성 배열 비워서 title이 업데이트되지 않도록 해야함
   }, []);
 
   useEffect(() => {
     if (titleRef.current) titleRef.current.focus();
   }, [isDoubleClicked]);
 
-  const onInputTitle = useCallback((e: React.ChangeEvent<HTMLDivElement>) => {
-    e.stopPropagation();
+  const onInputTitle = useCallback(
+    (e: React.ChangeEvent<HTMLDivElement>) => {
+      e.stopPropagation();
 
-    updateTodo({ id, title: e.target.innerText });
-  }, []);
+      updateTodo({ id, title: e.target.innerText });
+    },
+    [updateTodo]
+  );
 
   return (
     <Container
