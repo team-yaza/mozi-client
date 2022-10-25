4d1abfbe4feb27eb4f4b2710dcea472a0fb3661c
hyunjin <leehj0110@kakao.com>
2022-10-25 20:18:52 +0900

    chore: figure에서 fill 속성 제거

--- src/components/common/Figure/BOXARROWRIGHT.tsx
+++ src/components/common/Figure/BOXARROWRIGHT.tsx
@@ -7,7 +7,6 @@ const BOXARROWRIGHT: React.FC<FigureProps> = () => {
       <svg width="34" height="34" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path
           d="M21 4H13C8.02944 4 4 8.02944 4 13V21C4 25.9706 8.02944 30 13 30H21C25.9706 30 30 25.9706 30 21V13C30 8.02944 25.9706 4 21 4Z"
-          fill="white"
           strokeWidth="2"
         />
         <path d="M18.5 13L22.5 17L18.5 21" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
--- src/components/common/Figure/BOXPLUS.tsx
+++ src/components/common/Figure/BOXPLUS.tsx
@@ -7,7 +7,6 @@ const BOXPLUS: React.FC<FigureProps> = () => {
       <svg width="34" height="34" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path
           d="M21 4H13C8.02944 4 4 8.02944 4 13V21C4 25.9706 8.02944 30 13 30H21C25.9706 30 30 25.9706 30 21V13C30 8.02944 25.9706 4 21 4Z"
-          fill="white"
           strokeWidth="2"
         />
         <path d="M12.334 17H21.667" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
