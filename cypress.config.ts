fdb696262287ba4b7239e3bc266618e317e311c0
HYUNJIN LEE <63354527+hyunjinee@users.noreply.github.com>
2022-11-01 19:37:27 +0900

    chore: cypress ci (#445)

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,9 +2,12 @@
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
+  projectId: 'q4deia',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: 'http://localhost:3000/',
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
   },
 });
