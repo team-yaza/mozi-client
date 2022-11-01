f92b4a8f8cdc7e24089679806fff3f4df4e640ec
HYUNJIN LEE <63354527+hyunjinee@users.noreply.github.com>
2022-11-01 18:47:46 +0900

    chore: TodoListItemDescription Storybook 인자 만듦 (#444)

--- src/components/common/TodoListItem/Description/index.stories.tsx
+++ src/components/common/TodoListItem/Description/index.stories.tsx
@@ -11,3 +11,15 @@ export default {
 const Template: ComponentStory<typeof TodoListItemDescription> = (args) => <TodoListItemDescription {...args} />;
 
 export const Default = Template.bind({});
+
+Default.args = {
+  todo: {
+    id: '1',
+    title: 'Todo Title',
+    description: 'Todo Description',
+    index: 1,
+    done: false,
+    alarmed: false,
+    createdAt: new Date(),
+  },
+};
