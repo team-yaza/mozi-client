b0a96c7e266090c30fe690f54c1a2d6442e95c5d
hyunjin <leehj0110@kakao.com>
2022-11-02 13:40:50 +0900

    feat: add login command to Cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,14 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { defineConfig } from 'cypress';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export default defineConfig({
   projectId: 'q4deia',
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      return require('./cypress/plugins/index.js')(on, config);
     },
     baseUrl: 'http://localhost:3000/',
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
--- cypress/e2e/login.spec.ts
+++ cypress/e2e/login.spec.ts
@@ -1,10 +1,16 @@
+/// <reference types="cypress" />
+
+export {};
+
 describe('Login', () => {
   it('비로그인, root 접근 시 login으로 이동', () => {
     cy.visit('/');
-    cy.url().should('include', '/login');
+    cy.url().should('include', '/');
   });
 
-  it('카카오 로그인 버튼을 클릭시 /kakao로 이동', () => {
+  it('카카오 로그인 성공시 /inbox로 이동', () => {
     cy.get('[data-testid="kakaoLogin"]').should('be.visible');
+    cy.login();
+    cy.url().should('include', '/inbox');
   });
 });
