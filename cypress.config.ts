13a0f8ca0d4948c20d803df37f328a72b9dfe742
HYUNJIN LEE <63354527+hyunjinee@users.noreply.github.com>
2022-11-02 20:42:49 +0900

    refactor: 분리된 CI 파일 통합  (#451)
    * chore: CI 파일 통합
    
    * chore: cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,14 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { defineConfig } from 'cypress';
+import cypress, { defineConfig } from 'cypress';
 
 export default defineConfig({
   projectId: 'q4deia',
   e2e: {
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config);
+      // Cypress.env('TOKEN', config.env.TOKEN);
+
+      console.log(config.env.TOKEN);
+      return config;
     },
     baseUrl: 'http://localhost:3000/',
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
