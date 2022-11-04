8dc8c9f7d5f7995a244488f290630cea7cb6a528
HYUNJIN LEE <63354527+hyunjinee@users.noreply.github.com>
2022-11-04 14:04:33 +0900

    refactor: queryKey (#454)
    * chore: queryKey constant로 분리
    
    * refactor: queryKey 상수 도입 및 사용하지 않는 쿼리 삭제

--- src/hooks/apis/todo/useTodoListQuery.ts
+++ src/hooks/apis/todo/useTodoListQuery.ts
@@ -1,4 +1,3 @@
-import { useCallback } from 'react';
 import { AxiosError } from 'axios';
 import { ServerResponse } from 'http';
 import { useQuery, UseQueryResult } from '@tanstack/react-query';
@@ -8,49 +7,35 @@ import { Todo, TodoStatistics } from '@/shared/types/todo';
 import todoService from '@/services/apis/todo';
 import { ROUTES } from '@/shared/constants/routes';
 import { QUERY_TYPE_ERROR } from '@/shared/constants/dialog';
+import { queryKeys } from '@/shared/constants/queryKey';
 
 export const useTodoListQuery = (page: string): UseQueryResult<Todo[], AxiosError<ServerResponse>> => {
   switch (page) {
     case ROUTES.HOME:
-      return useQuery(['todos'], todoService.getTodosFromIndexedDB, {
+      return useQuery([queryKeys.GET_TODOLIST], todoService.getTodosFromIndexedDB, {
         select: (todos: Todo[]) =>
           todos.filter((todo) => !todo.done && !todo.deletedAt).sort((todoA, todoB) => todoA.index - todoB.index),
       });
     case ROUTES.UPCOMING:
-      return useQuery(['todos'], todoService.getTodosFromIndexedDB, {
+      return useQuery([queryKeys.GET_TODOLIST], todoService.getTodosFromIndexedDB, {
         select: (todos: Todo[]) => todos.filter((todo) => (todo.dueDate || todo.alarmDate) && !todo.deletedAt),
       });
     case ROUTES.LOGBOOK:
-      return useQuery(['todos'], todoService.getTodosFromIndexedDB, {
+      return useQuery([queryKeys.GET_TODOLIST], todoService.getTodosFromIndexedDB, {
         select: (todos: Todo[]) => todos.filter((todo) => todo.done && !todo.deletedAt),
       });
     case ROUTES.TRASH:
-      return useQuery(['todos'], todoService.getTodosFromIndexedDB, {
-        select: (todos: Todo[]) => todos.filter((todo) => todo.deletedAt),
+      return useQuery([queryKeys.GET_TODOLIST], todoService.getTodosFromIndexedDB, {
+        select: (todos: Todo[]) => todos.filter((todo) => todo.deletedAt && todo.offline !== 'forceDeleted'),
       });
     case ROUTES.MAP:
-      return useQuery(['todos'], todoService.getTodosFromIndexedDB, {
+      return useQuery([queryKeys.GET_TODOLIST], todoService.getTodosFromIndexedDB, {
         select: (todos: Todo[]) => todos.filter((todo) => todo.latitude && todo.longitude && !todo.deletedAt),
       });
     default:
       throw new Error(QUERY_TYPE_ERROR);
   }
 };
-
-export const useSoftDeletedTodoList = () => {
-  return useQuery(['todos', 'deleted'], todoService.getTodosFromIndexedDB, {
-    select: useCallback((todos: Todo[]) => todos.filter((todo) => todo.deletedAt && !todo.offlineForceDeleted), []),
-  });
-};
-
-export const use_unsafe_todoListQuery = (): UseQueryResult<Todo[], AxiosError<ServerResponse>> =>
-  useQuery(['todos'], todoService.getTodosFromIndexedDB, {
-    select: useCallback(
-      (todos: Todo[]) =>
-        todos.filter((todo) => !todo.deletedAt && !todo.done).sort((a: any, b: any) => a.index - b.index),
-      []
-    ),
-  });
 
 export const useTodoListStatistics = () => {
   return useQuery<any>(['statistics'], todoService.getTodos, {
