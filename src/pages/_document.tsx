09fdfd985fd354fd30d2f63ef9cb069c5a941939
hyunjin <leehj0110@kakao.com>
2022-11-05 20:38:31 +0900

    chore: 다양한 기기에대한 startup image link tag 추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -58,11 +58,59 @@ class MyDocument extends Document {
 
           <link rel="manifest" href="/manifest.json" crossOrigin="use-credentials" />
           <link rel="shortcut icon" href="/favicon.svg" type="image/svg" />
-
+          {/* iPhone Xr (828px x 1792px) */}
           <link
             rel="apple-touch-startup-image"
             media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)"
             href="/apple-launch-828x1792.png"
+          />
+          {/* iPhone Xs Max (1242px x 2688px)  */}
+          <link
+            rel="apple-touch-startup-image"
+            media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)"
+            href="/apple-launch-1242x2688.png"
+          />
+          {/* iPhone X, Xs (1125px x 2436px) */}
+          <link
+            rel="apple-touch-startup-image"
+            media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)"
+            href="/apple-launch-1125x2436.png"
+          />
+          {/* iPhone 8 Plus, 7 Plus, 6s Plus, 6 Plus (1242px x 2208px) */}
+          <link
+            rel="apple-touch-startup-image"
+            media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3)"
+            href="/apple-launch-1242x2208.png"
+          />
+          {/* iPhone 8, 7, 6s, 6 (750px x 1334px) */}
+          <link
+            rel="apple-touch-startup-image"
+            media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)"
+            href="/apple-launch-750x1334.png"
+          />
+          {/* iPad Pro 12.9" (2048px x 2732px) */}
+          <link
+            rel="apple-touch-startup-image"
+            media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)"
+            href="/apple-launch-2048x2732.png"
+          />
+          {/* iPad Pro 11” (1668px x 2388px) */}
+          <link
+            rel="apple-touch-startup-image"
+            media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)"
+            href="/apple-launch-1668x2388.png"
+          />
+          {/* iPad Pro 10.5" (1668px x 2224px) */}
+          <link
+            rel="apple-touch-startup-image"
+            media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)"
+            href="/apple-launch-1668x2224.png"
+          />
+          {/* iPad Mini, Air (1536px x 2048px) */}
+          <link
+            rel="apple-touch-startup-image"
+            media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)"
+            href="/apple-launch-1536x2048.png"
           />
 
           <Script strategy="beforeInteractive" src="https://developers.kakao.com/sdk/js/kakao.min.js" />
