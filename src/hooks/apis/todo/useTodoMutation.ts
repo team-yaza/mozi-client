050056bcb2c9ea67d32c59a1cb94b96dd53bacfd
hyunjin <leehj0110@kakao.com>
2022-11-07 15:53:54 +0900

    refactor: update type 추가

--- src/hooks/apis/todo/useTodoMutation.ts
+++ src/hooks/apis/todo/useTodoMutation.ts
@@ -1,24 +1,19 @@
-// import { AxiosError } from 'axios';
 // import { v4 as uuid } from 'uuid';
+import { AxiosError } from 'axios';
 import { useMutation } from '@tanstack/react-query';
 import * as Sentry from '@sentry/nextjs';
 
 import todoService from '@/services/apis/todo';
 import { syncTodos } from '@/shared/utils/sync';
 import { queryClient } from '@/shared/utils/queryClient';
-import {
-  Todo,
-  // TodoSuccessResponse,
-  TodoUpdateRequest,
-  TodoCreateRequest,
-} from '@/shared/types/todo';
+import { Todo, TodoUpdateRequest, TodoCreateRequest } from '@/shared/types/todo';
 import { queryKeys } from '@/shared/constants/queryKey';
 import { toastError } from '@/shared/utils/toast';
 import { TODO_CREATE_FAILED, TODO_UPDATE_FAILED } from '@/shared/constants/dialog';
 
 export const useCreateTodoMutation = () =>
-  useMutation(
-    ({ locationName, longitude, latitude, dueDate, title }: TodoCreateRequest) =>
+  useMutation<Todo, AxiosError, TodoCreateRequest>(
+    ({ locationName, longitude, latitude, dueDate, title }) =>
       todoService.createTodoAtIndexedDB({
         title,
         locationName,
@@ -44,8 +39,8 @@ export const useCreateTodoMutation = () =>
   );
 
 export const useUpdateTodoMutation = () =>
-  useMutation(
-    ({ ...rest }: TodoUpdateRequest) =>
+  useMutation<Partial<Todo>, AxiosError, TodoUpdateRequest>(
+    ({ ...rest }) =>
       todoService.updateTodoAtIndexedDB({
         ...rest,
       }),
