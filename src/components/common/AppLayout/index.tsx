9f45f96f2990495cf253690d2cc80a6ba0960eb8
hyunjin <leehj0110@kakao.com>
2022-11-11 00:54:01 +0900

    chore: import 구문 정리

--- src/components/common/AppLayout/index.tsx
+++ src/components/common/AppLayout/index.tsx
@@ -1,6 +1,7 @@
+import { PropsWithChildren } from 'react';
+
 import Sidebar from '@/components/common/Sidebar';
 import { useTodoListStatistics } from '@/hooks/apis/todo/useTodoListQuery';
-import { PropsWithChildren } from 'react';
 import { Container } from './styles';
 
 const AppLayout: React.FC<PropsWithChildren> = ({ children }) => {
