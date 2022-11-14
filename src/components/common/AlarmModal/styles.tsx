32fd9c3582658f19ea38841996a99ce06f810bfa
HYUNJIN LEE <63354527+hyunjinee@users.noreply.github.com>
2022-11-14 12:43:53 +0900

    style: Alarm Styling (#514)

---
+++ src/components/common/AlarmModal/styles.tsx
@@ -0,0 +1,78 @@
+import { theme } from '@/styles/theme';
+import { flexCenter } from '@/styles/utils';
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  padding-inline: 1rem;
+
+  /* justify-content: center; */
+`;
+
+export const Title = styled.h2`
+  ${theme.fonts.h2};
+  margin-block: 2rem;
+`;
+
+export const Place = styled.h3`
+  align-self: flex-start;
+  /* margin-left: 1rem; */
+  ${theme.fonts.h3};
+  font-weight: 500;
+
+  margin-bottom: 1rem;
+`;
+
+export const Time = styled.h3`
+  align-self: flex-start;
+  /* margin-left: 1rem; */
+  ${theme.fonts.h3};
+  font-weight: 500;
+`;
+
+export const Option = styled.h3`
+  align-self: flex-start;
+  /* margin-left: 1rem; */
+  ${theme.fonts.h3};
+  font-weight: 500;
+
+  margin-bottom: 1rem;
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-around;
+
+  margin-bottom: 1rem;
+`;
+
+export const Select = styled.select`
+  width: 100%;
+  outline: none;
+
+  margin-inline: 2rem;
+  margin-bottom: 1rem;
+  padding-block: 1rem;
+
+  font-size: 1.6rem;
+`;
+
+export const Button = styled.button`
+  ${flexCenter};
+  width: 8rem;
+  padding: 1.5rem;
+  font-size: 1.6rem;
+  border-radius: 1rem;
+
+  outline: none;
+  border: none;
+
+  color: black;
+  background-color: ${theme.colors.lightPurple};
+  cursor: pointer;
+`;
