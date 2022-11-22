e2a80c60b73a3fc5e011659224f3bdb845b0ecca
hyunjin <leehj0110@kakao.com>
2022-11-22 13:22:24 +0900

    fix: safari 브라우저에서 svg 렌더링 잘 되지 않는 현상

--- src/components/common/Figure/CALENDAR.tsx
+++ src/components/common/Figure/CALENDAR.tsx
@@ -3,7 +3,7 @@ import { SvgWrap } from './styles';
 const CALENDAR: React.FC = () => {
   return (
     <SvgWrap>
-      <svg viewBox="0 0 23 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <svg viewBox="0 0 23 23" fill="none" xmlns="http://www.w3.org/2000/svg" width="25" height="25">
         <path
           d="M17 5H6C4.89543 5 4 5.89543 4 7V17C4 18.1046 4.89543 19 6 19H17C18.1046 19 19 18.1046 19 17V7C19 5.89543 18.1046 5 17 5Z"
           strokeLinecap="round"
--- src/components/common/Figure/DELETE.tsx
+++ src/components/common/Figure/DELETE.tsx
@@ -4,7 +4,7 @@ import { FigureProps } from '@/shared/types/figure';
 const DELETE: React.FC<FigureProps> = ({ stroke = 'white' }) => {
   return (
     <SvgWrap>
-      <svg stroke="none" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+      <svg stroke="none" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg" width="12" height="12">
         <path
           d="M6 12C9.31371 12 12 9.31371 12 6C12 2.68629 9.31371 0 6 0C2.68629 0 0 2.68629 0 6C0 9.31371 2.68629 12 6 12Z"
           fill="#CBCBCB"
